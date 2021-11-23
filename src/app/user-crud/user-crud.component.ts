04759bb482747e0c2b7df1d1b1078c358262662d
Shaikh Irshad <shaikhij2000@gmail.com>
2021-11-23 08:53:40 +0530

    route updates

--- src/app/user-crud/user-crud.component.ts
+++ src/app/user-crud/user-crud.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-crud',
@@ -11,12 +11,36 @@ export class UserCrudComponent implements OnInit {
 
   usersArray: any = []
   users: FormGroup
+  appState: any = {
+    new: true,
+    edit: false,
+    delete: false
+  }
+  activeUser: any = null
 
-  constructor(private fb: FormBuilder, private route: Router) {
+  constructor(private fb: FormBuilder, private route: Router, private activatedRoute: ActivatedRoute) {
     this.users = this.fb.group({
       name: ['', [Validators.required]]
     })
     this.usersArray = this.getLocalStorage()
+    if (this.activatedRoute.snapshot.params.editId) {
+      this.appState = {
+        new: false,
+        edit: true,
+        delete: false
+      }
+      this.activeUser = this.usersArray.find((x: any) => x.id == this.activatedRoute.snapshot.params.editId)
+      this.users.patchValue({
+        name: this.activeUser.name
+      })
+    } else if (this.activatedRoute.snapshot.params.deleteId) {
+      this.appState = {
+        new: false,
+        edit: false,
+        delete: true
+      }
+      this.activeUser = this.usersArray.find((x: any) => x.id == this.activatedRoute.snapshot.params.deleteId)
+    }
   }
 
   ngOnInit(): void {
@@ -25,6 +49,11 @@ export class UserCrudComponent implements OnInit {
   navigateToDelete(id: number) {
     this.route.navigate([`users/delete/${id}`])
   }
+
+  navigateToEdit(id: number) {
+    this.route.navigate([`users/edit/${id}`])
+  }
+
 
   setLocalStorage() {
     localStorage.setItem('users', JSON.stringify(this.usersArray))
@@ -36,17 +65,31 @@ export class UserCrudComponent implements OnInit {
   }
 
   addUsers() {
-    this.usersArray.push({
-      ...this.users.value,
-      id: this.usersArray.length + 1
-    })
+    if (this.activeUser) {
+      this.updateUser()
+    }
+    else {
+      this.usersArray.push({
+        ...this.users.value,
+        id: this.usersArray.length + 1
+      })
+      this.setLocalStorage()
+      this.users.reset()
+      this.route.navigate(['/users'])
+    }
+  }
+
+  updateUser() {
+    this.usersArray.find((x: any) => x.id == this.activeUser.id).name = this.users.get('name')?.value
     this.setLocalStorage()
     this.users.reset()
+    this.route.navigate(['/users'])
   }
 
   deleteUser(id: number) {
     this.usersArray = this.usersArray.filter((x: any) => x.id != id)
     this.setLocalStorage()
+    this.route.navigate(['/users'])
   }
 
 }
